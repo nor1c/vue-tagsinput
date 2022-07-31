c67f90647782dc684f47c8b307348ecc91a160b2
Ahmad Fauzi <ahmad.uji1902@gmail.com>
2022-07-31 19:05:15 +0700

    fix: attempt to fix modelValue issue

---
+++ test/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <TagInput 
+    v-model='tags'
+    :placeholder="'Type a tag'"
+  />
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import TagInput from '../../src/App.vue'
+
+const tags = ref([])
+</script>
